ee1d2e5b099fb362e13f75c1aedf013b5a2fd7da
Wilka Carvalho <wcarvalho92@gmail.com>
2024-04-23 13:05:45 -0400

    episode optionally terminates when any pair object is picked up

--- projects/humansf/keyroom_symbolic.py
+++ projects/humansf/keyroom_symbolic.py
@@ -24,10 +24,10 @@ class SymbolicKeyRoomEnvParams(KeyRoomEnvParams):
 
 class KeyRoomSymbolic(KeyRoom):
 
-    def __init__(self, test_episodes_ends_on_key_pickup: bool = False, name='keyroom'):
+    def __init__(self, test_episodes_end_on_key_pickup: bool = False, name='keyroom'):
         super().__init__()
         self.name = name
-        self.test_episodes_ends_on_key_pickup = test_episodes_ends_on_key_pickup
+        self.test_episodes_end_on_key_pickup = test_episodes_end_on_key_pickup
 
     def time_limit(self, params: EnvParams) -> int:
         return 20
