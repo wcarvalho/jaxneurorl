109f4b418f05cbf8952fed00f132257a1b3be7b9
Wilka Carvalho <wcarvalho92@gmail.com>
2024-05-15 13:19:01 -0400

    seems to be working... now to test on gcloud

--- projects/online_dyna/wsgi.py
+++ projects/online_dyna/wsgi.py
@@ -1,5 +1,7 @@
+import os
 from main import socketio, app
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=8080, debug=False)
+    #socketio.run(app, host='0.0.0.0', port=8080, debug=False)
+    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=False)
     #app.run(host='0.0.0.0', port=8080)
